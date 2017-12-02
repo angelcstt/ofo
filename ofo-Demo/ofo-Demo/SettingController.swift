a8b9e187f7999d02fde0710fea9ea8dd99d62237
macosx <macosx@macosxdemac.local>
2017-12-02 12:23:46 +0800

    引入第三方库 完成侧滑和首页底部button布局

--- ofo-Demo/ofo-Demo/SettingController.swift
+++ ofo-Demo/ofo-Demo/SettingController.swift
@@ -1,37 +1,43 @@
 //
-//  ViewController.swift
+//  SettingController.swift
 //  ofo-Demo
 //
-//  Created by 蔡四堂 on 2017/12/1.
+//  Created by macosx on 2017/12/2.
 //  Copyright © 2017年 angelcstt. All rights reserved.
 //
 
 import UIKit
 import SWRevealViewController
 
-class ViewController: UIViewController {
+class SettingController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.navigationItem.leftBarButtonItem?.image = #imageLiteral(resourceName: "user_center_icon").withRenderingMode(.alwaysOriginal)
-        self.navigationItem.rightBarButtonItem?.image = #imageLiteral(resourceName: "gift_icon").withRenderingMode(.alwaysOriginal)
-        self.navigationItem.titleView = UIImageView(image: #imageLiteral(resourceName: "Login_Logo"))
-        self.navigationItem.backBarButtonItem = UIBarButtonItem(title: "", style: .plain, target: nil, action: nil)
+        
         if let revealVC = revealViewController() {
             navigationItem.leftBarButtonItem?.target = revealVC
             revealVC.rearViewRevealWidth = 310
             navigationItem.leftBarButtonItem?.action = #selector(SWRevealViewController.revealToggle(_:))
             view.addGestureRecognizer(revealVC.panGestureRecognizer())
         }
-        
-        
+
+        // Do any additional setup after loading the view.
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
 
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
 
 }
-
